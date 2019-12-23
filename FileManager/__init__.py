de048a2d5d4e5817456ee000a9abd5259b9f6510
javad <javadasoodeh73@gmail.com>
2019-12-23 13:10:49 +0330

    - filemanager complete v 1.0.0

--- FileManager/__init__.py
+++ FileManager/__init__.py
@@ -1,30 +1,61 @@
 #!/usr/bin/python
 from pyramid.config import Configurator  # boot
-from .classes.config import Config
-from .models.meta import MyRequest
+from FileManager.classes.config import Config
+from FileManager.models.meta import MyRequest
+import configparser
+import tornado.ioloop
+import tornado.wsgi
+from tornado.httpserver import HTTPServer
+
+__author__ = 'javadasoodeh73@gmail.com'
+
+# read ini file
+settings = configparser.ConfigParser()
+settings.read("D:\\projects\\file-manager\\configs\\config\\FileManager.ini")
+main_settings = dict(settings.items('app:main'))
+server_settings = dict(settings.items('server:main'))
 
 
-def main(global_config, **settings):
+
+
+def main(**settings):
     # create object of configurator
-    config = Configurator(settings=settings)
-    config.set_request_factory(MyRequest)
+    with Configurator(settings=settings) as config:
+        # create object of configurator
 
-    # settings
-    settings = config.registry.settings
+        config.set_request_factory(MyRequest)
 
-    # urls
-    url_location = settings['setting_folder'] + '/to_srv_urls/FileManager.properties'
+        # settings
+        settings = config.registry.settings
 
-    get_urls = Config(properties_file_path=url_location)
-    urls = get_urls.urls_()
+        # urls
+        url_location = settings['setting_folder'] + '/to_srv_urls/FileManager.properties'
 
-    for route in urls:
-        config.add_route(route, urls[route])
+        get_urls = Config(properties_file_path=url_location)
+        urls = get_urls.urls_()
 
-    # scanning views
-    config.scan('FileManager.views')
+        for route in urls:
+            config.add_route(route, urls[route])
 
-    """ create a web server / host name && port && ... """
-    app = config.make_wsgi_app()
-    application = app
-    return application
+        # scanning views
+        config.scan('FileManager.views')
+
+        """ create a web server / host name && port && ... """
+        app = config.make_wsgi_app()
+        application = app
+        return application
+
+
+
+from tornado.autoreload import add_reload_hook, start
+
+if __name__ == '__main__':
+    c = tornado.wsgi.WSGIContainer(main(**main_settings))
+    http_server = HTTPServer(c)
+    http_server.listen(int(server_settings.get('port')))
+    start()
+
+    tornado.ioloop.IOLoop.instance().start()
+    # server = make_server(server_settings['host'], int(server_settings['port']), main(**main_settings))
+    # server.serve_forever()
+
